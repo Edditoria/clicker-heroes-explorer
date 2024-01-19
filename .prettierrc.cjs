8b9cd7bef4fa25a5ce621d78d6f62388a794d48b
Edditoria <edditoria@pm.me>
2024-01-19 17:17:02 +0800

    website: Move SvelteKit dir to repo root; Update Svelte config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,7 +2,9 @@ module.exports = {
 	useTabs: true,
 	singleQuote: true,
 	jsxSingleQuote: false, // as default.
-	trailingComma: 'es5', // as default.
+	trailingComma: 'es5', // as default. SvelteKit default: 'none'.
 	bracketSpacing: true, // as default.
-	printWidth: 999,
+	printWidth: 999, // SvelteKit default: 100.
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
 };
--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,22 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		files: {
+			assets: 'website/static',
+			hooks: {
+				client: 'website/hooks.client',
+				server: 'website/hooks.server',
+				universal: 'website/hooks',
+			},
+			lib: 'website/lib',
+			params: 'website/params',
+			routes: 'website/routes',
+			serviceWorker: 'website/service-worker',
+			appTemplate: 'website/app.html',
+			errorTemplate: 'website/error.html',
+		},
+	},
 };
 
 export default config;
